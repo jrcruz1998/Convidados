af35b98089d0ffd8b2be0271f8df0a0854c81b44
Jonathas Cruz <jonathas-cruz@hotmail.com>
2023-05-23 18:34:39 -0300

    Mudancas na exibicao dos convidados no RecyclerView

--- app/src/main/java/com/example/convidados/view/AllGuestsFragment.kt
+++ app/src/main/java/com/example/convidados/view/AllGuestsFragment.kt
@@ -5,12 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.convidados.databinding.FragmentAllGuestsBinding
 import com.example.convidados.view.adapter.GuestsAdapter
+import com.example.convidados.view.listener.OnGuestListener
 import com.example.convidados.viewModel.AllGuestsViewModel
 
 class AllGuestsFragment : Fragment() {
@@ -31,6 +33,18 @@ class AllGuestsFragment : Fragment() {
 
         // Adapter
         binding.recyclerAllGuests.adapter = adapter
+
+        val listener = object : OnGuestListener {
+            override fun onClick(id: Int) {
+                Toast.makeText(context, "Alow, fui clicado", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onDelete(id: Int) {
+            }
+
+        }
+
+        adapter.attachListener(listener)
 
         viewModel.getAll()
 
--- app/src/main/java/com/example/convidados/view/adapter/GuestsAdapter.kt
+++ app/src/main/java/com/example/convidados/view/adapter/GuestsAdapter.kt
@@ -5,18 +5,27 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.convidados.databinding.RowGuestBinding
 import com.example.convidados.model.GuestModel
+import com.example.convidados.view.listener.OnGuestListener
 import com.example.convidados.view.viewholder.GuestViewHolder
 
 class GuestsAdapter : RecyclerView.Adapter<GuestViewHolder>() {
 
+    companion object {
+        var countCreate = 0
+        var countBind = 0
+    }
+
     private var guestList: List<GuestModel> = listOf()
+    private lateinit var listener: OnGuestListener
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GuestViewHolder {
+        countCreate++
         val item = RowGuestBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return GuestViewHolder(item)
+        return GuestViewHolder(item, listener)
     }
 
     override fun onBindViewHolder(holder: GuestViewHolder, position: Int) {
+        countBind++
         holder.bind(guestList[position])
     }
 
@@ -28,5 +37,9 @@ class GuestsAdapter : RecyclerView.Adapter<GuestViewHolder>() {
         guestList = list
         // Notifica para que realize as atualizações
         notifyDataSetChanged()
+    }
+
+    fun attachListener(guestListener: OnGuestListener) {
+        listener = guestListener
     }
 }
